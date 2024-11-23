fc4ca9fc01a62711ffecc8816ad4019b5a13d449
Luiz Felipe <lfelipelopesti@gmail.com>
2024-11-23 16:29:16 -0300

    config trus proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
 
         $middleware->validateCsrfTokens(['/buy-credits/webhook']);
+        $middleware->trustProxies(at: '*');
 
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
